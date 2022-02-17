52127b2e417871dc9ee136a5d0a2c37f16545869
Artem Syvak <art.syvak@gmail.com>
2022-02-17 20:39:32 +0200

    Showreel section :: done.

--- src/components/Header/NavigationLinks/index.tsx
+++ src/components/Header/NavigationLinks/index.tsx
@@ -1,13 +1,41 @@
 import styles from './NavigationLinks.module.scss';
 
+interface LinkType{
+    label: string,
+    url: string
+}
+
+const Links: LinkType[] = [
+    {
+        url: '#',
+        label: 'Про Нас'
+    },
+    {
+        url: '#',
+        label: 'Роботи'
+    },
+    {
+        url: '#',
+        label: 'Послуги'
+    },
+    {
+        url: '#',
+        label: 'Контакти'
+    },
+    {
+        url: '#',
+        label: 'Замовити Проект'
+    }
+]
+
 const NavigationLinks = () => {
     return (
-        <ul className={styles.navigationLinks}>
-            <li><a href="#">Про нас </a></li>           
-            <li><a href="#">Роботи</a></li>
-            <li><a href="#">Послуги</a></li>
-            <li><a href="#">Контакти</a></li>
-            <li><a href="#">Замовити проект</a></li>
+        <ul className={`${styles.navigationLinks} mb-0`}>
+            {Links.map((link: LinkType) => (
+                <li key={link.label}>
+                    <a href={link.url}>{link.label}</a>   
+                </li>
+            ))}
         </ul>
     )
 }
--- src/components/Showreel/index.tsx
+++ src/components/Showreel/index.tsx
@@ -1,5 +1,4 @@
 import Button from '../Button';
-
 import styles from './Showreel.module.scss';
 
 const Showreel = () => {
@@ -10,28 +9,29 @@ const Showreel = () => {
     }
 
     return (
-        <div className={styles.showreel}> 
-            <span className={styles.shadow}></span>      
-            <h1>parsec studio</h1>
-            <Button 
-                style={{marginTop: '20px'}}
+        <div className={styles.showreel}>
+            <span className={styles.shadow}></span>
+            <img src="/showreel-logo.svg" className={styles.showreelLogo} alt="" />
+            <Button
+                style={{ marginTop: '20px' }}
                 icon="/play-icon.svg"
                 label='Watch showreel'
                 onClick={playShowreel}
-            /> 
-            <div className={styles.footer}>
-                <p>
-                Ми допомагаємо брендам говорити з людьми,
-а молодим артистам — доповнювати музичні
-ідеї картинкою. 
-                </p>
-                <div className={styles.skipButton}></div>
-                <ul className={styles.tags}>
-                    <li><a href="#">Реклама</a></li>
-                    <li><a href="#">Кліпи</a></li>
-                    <li><a href="#">Комерція</a></li>
-                </ul>
-            </div>  
+            />      
+
+                <div className={`${styles.footer} container gx-0`}>                   
+                    <p>
+                        Ми допомагаємо брендам говорити з людьми,
+                        а молодим артистам — доповнювати музичні
+                        ідеї картинкою.
+                    </p>
+                    <div className={styles.skipButton}></div>
+                    <ul className={styles.tags}>
+                        <li><a href="#">Реклама</a></li>
+                        <li><a href="#">Анімація</a></li>
+                        <li><a href="#">Кліпи</a></li>
+                    </ul>                   
+                </div>            
         </div>
     )
 }
