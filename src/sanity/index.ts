97e00c164d66ea7509cf051df92905e5440586c6
Artem Syvak <art.syvak@gmail.com>
2022-03-30 20:05:47 +0300

    - Sanity setup for services section v2.

--- src/sanity/index.ts
+++ src/sanity/index.ts
@@ -4,6 +4,5 @@ export default sanityClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
     dataset: "production",
     apiVersion: '2021-08-31',
-    token: '',
     useCdn: true,
   });
