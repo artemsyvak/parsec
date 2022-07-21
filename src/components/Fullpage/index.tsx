a4c11ea068747964d968040e29bf93b7b8a799cb
Artem Syvak <artsyvak@gmail.com>
2022-07-21 18:44:33 +0300

    - scroll wheel handler.

--- src/components/Fullpage/index.tsx
+++ src/components/Fullpage/index.tsx
@@ -1,144 +1,42 @@
-import React, { FunctionComponent, PropsWithChildren, ReactNode, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useState, useRef } from "react";
-import { useEventListener, useDebounce, usePrevious } from "../../hooks";
-import throttle from 'lodash/throttle';
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 
 
 import styles from './Fullpage.module.scss'
 
-const directions = {
-    FORWARD: 'FORWARD',
-    BACKWARD: 'BACKWARD',
-    NONE: 'NONE'
-}
-
-let scrolling = false
-
 const Fullpage = (props: any) => {
 
-    const { children } = props;
-    const count = children && children.length ? children.length : 0;
-
     const container = useRef(null)
-    const [stateDirection, setDirection] = useState(directions.NONE)
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [nextIndex, setNextIndex] = useState(count ? 1 : 0)
-    const [prevIndex, setPrevIndex] = useState(count ? count - 1 : 0) 
-    // const [scrolling, setScrolling] = useState(false)
-    // const debounceIndex = useDebounce(currentIndex, )    
+
+    useEffect(() => {
+        container.current.style.transform = `translateY(-${100 * currentIndex}vh)` 
+    }, [currentIndex])
 
 
-    const handleScroll = (e: any) => { 
-        e.preventDefault();
-        const { deltaY } = e;        
-
-        let direction = directions.NONE;
-        if (deltaY < 0) {
-          direction = directions.FORWARD
+    const moveUp = () => {
+        if(currentIndex !== 0){
+            setCurrentIndex(currentIndex - 1)
         }
-        if (deltaY > 0) {
-          direction = directions.BACKWARD
-        }
-        setNextSlideThrottled(direction);         
     }
 
-    const setNextSlide = (direction: any) => {        
-        const {children = null} = props
-        if (!children || children.length <= 1) {
-            return null;
+    const moveDown = () => {
+        if(currentIndex < props.children.length - 1){
+            setCurrentIndex(currentIndex + 1)
         }
-
-        const count = children.length;
-        let newIndex = 0
-
-        if (direction === directions.NONE) {
-            return null;
-        }
-
-        if (direction === directions.FORWARD) {
-            if (currentIndex < count - 1) {
-              newIndex = currentIndex + 1;
-            } else {
-              newIndex = 0;
-            }
-        }
-
-        if (direction === directions.BACKWARD) {
-            if (currentIndex === 0) {
-              newIndex = count - 1;
-            } else {
-              newIndex = currentIndex - 1;
-            }
-        }
-
-        let newNextIndex = newIndex === count - 1 ? 0 : newIndex + 1;
-        let newPrevIndex = newIndex === 0 ? count - 1 : newIndex - 1;
-
-        // newIndex = currentIndex === 0 && nextIndex > 1 ? 0 : newIndex
-
-        setCurrentIndex(newIndex)
-        setNextIndex(newNextIndex)
-        setPrevIndex(newPrevIndex)       
-
-        const koef = direction === directions.BACKWARD ? 1 : -1
-
-        if(!scrolling){
-
-            window.requestAnimationFrame(() => {
-                console.log('animation frame')
-                window.scrollBy({
-                    top: window.innerHeight * koef ,
-                    left: 0,
-                    behavior: 'smooth'
-                })
-                scrolling = false
-            })
-
-            console.log('animation frame end')
-
-            scrolling = true
-        }
-        
-
-        console.log('scrolling:', scrolling, 'current:', currentIndex, 'prev:', prevIndex, 'next:',  nextIndex)
     }
-
-    const setNextSlideThrottled = throttle(setNextSlide, 1000, {
-        leading: true,
-        trailing: false,
-    })
-
-    useEffect(() => {        
-        window.removeEventListener('mousewheel', handleScroll)
-        window.addEventListener('mousewheel', handleScroll, {passive: false})
-        return () => window.removeEventListener('mousewheel', handleScroll)
-    }, [])
-
-    // const [offset, setOffset] = useState(0);
-
-    // const updateOffset = () => {
-    //     setOffset(window.pageYOffset)
-    // }
-
-    // const updateThrottleOffset = throttle(updateOffset, 1000, {
-    //     leading: true,
-    //     trailing: false
-    // })
-
-    // useEffect(() => {
-    //     const onScroll = () => updateThrottleOffset()
-    //     // clean up code
-    //     window.removeEventListener('scroll', onScroll);
-    //     window.addEventListener('scroll', onScroll, { passive: true });
-    //     return () => window.removeEventListener('scroll', onScroll);
-    // }, []);
-
-    // console.log(offset); 
 
     return (
-        <div className={styles.fullpageContainer}>
-            {props.children}
-        </div>        
+        <div className={styles.fullpageContainer} ref={container}>
+            <ReactScrollWheelHandler
+                upHandler={moveUp}
+                downHandler={moveDown}
+                timeout={700}
+            >
+                {props.children}
+            </ReactScrollWheelHandler>
+        </div>
     )
 }
 
