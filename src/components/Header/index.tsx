7525d6ba264a70538fdbd7894d86e2a4d36e5fcf
Artem Syvak <artsyvak@gmail.com>
2022-07-21 18:41:31 +0300

    Showreel section :: done.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
-
+import { Container, Row, Col } from 'react-bootstrap'
 import NavigationLinks from './NavigationLinks';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <Image src="/logo.svg" alt="Parsec Studio Logo" width={159} height={17} />
-            <NavigationLinks/>                   
-        </div>
 
+        <Container className={`${styles.header}`}>
+            <Row className="justify-content-space-between align-items-start h-100 gx-0">
+                <Col xl={2} className="gx-0">
+                    <Image src="/logo.svg" alt="Parsec Studio Logo" width={159} height={17} />
+                </Col>
+                <Col xl={10} className="gx-0 d-flex justify-content-end">
+                    <NavigationLinks />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
